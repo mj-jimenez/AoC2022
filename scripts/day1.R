eaf36e7093b7ffe23f4d4a18bf05a6fd3cc5dbfb
Maria Jose Jimenez-Santos <mj.jimenezsantos@outlook.com>
2022-12-01 11:58:14 +0100

    Solve Day 1 puzzle

---
+++ scripts/day1.R
@@ -0,0 +1,25 @@
+rm(list = ls())
+
+# --- Data ---
+calories <- scan("data/day1_1.txt", what = numeric(), blank.lines.skip = FALSE)
+
+# --- Code ---
+# NA values (indicate separation from and elf to another)
+na_values <- which(is.na(calories))
+
+# Start and end positions for each elf in the calories vector
+ranges <- data.frame(start = c(1, na_values + 1),
+                     end = c(na_values - 1, length(calories)))
+
+# Split calories vector to a vector with each elf's calories
+elf_calories <- sapply(1:nrow(ranges), FUN = function(i) {
+  thisline <- ranges[i, ]
+  sum(calories[seq(thisline$start, thisline$end)])
+})
+
+# --- Answers ---
+# Max calories carried by 1 elf
+max(elf_calories)
+
+# Total calories carried by the top 3 elves
+sum(sort(elf_calories, decreasing = TRUE)[1:3])
