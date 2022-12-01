517fae786fdf87ac3fff6d2881407f680ede9197
mjjimenez <51485618+mj-jimenez@users.noreply.github.com>
2022-12-01 12:04:41 +0100

    Update comments

--- scripts/day1.R
+++ scripts/day1.R
@@ -4,14 +4,14 @@ rm(list = ls())
 calories <- scan("data/day1_1.txt", what = numeric(), blank.lines.skip = FALSE)
 
 # --- Code ---
-# NA values (indicate separation from and elf to another)
+# NA values (indicate separation from an elf to another)
 na_values <- which(is.na(calories))
 
 # Start and end positions for each elf in the calories vector
 ranges <- data.frame(start = c(1, na_values + 1),
                      end = c(na_values - 1, length(calories)))
 
-# Split calories vector to a vector with each elf's calories
+# Split calories to a vector with each elf's calories
 elf_calories <- sapply(1:nrow(ranges), FUN = function(i) {
   thisline <- ranges[i, ]
   sum(calories[seq(thisline$start, thisline$end)])
